c149528c7549414c04f7b13cf3142e9f261fd42b
wwhhff <s1977764@ed.ac.uk>
2023-02-09 21:30:16 +0000

    initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public CarAgent carAgent;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("zone"))
+        {
+            carAgent.AddReward(10);
+            carAgent.EndEpisode();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("wall"))
+        {
+            carAgent.AddReward(-10);
+            carAgent.EndEpisode();
+        }
+    }
+}
---
+++ Assets/Scripts/CarAgent.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+using VehicleBehaviour;
+using System.IO.Compression;
+
+public class CarAgent : Agent
+{
+    public WheelVehicle wheelVehicle;
+    public Rigidbody rb;
+
+    public Transform ball;
+    public Rigidbody ball_rb;
+    public Transform zone;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-30.0f, 30.0f), 0, Random.Range(-30.0f, 30.0f));
+        transform.localRotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        Vector3 ballpos = new Vector3(Random.Range(-30.0f, 30.0f), 1.5f, Random.Range(-30.0f, 30.0f));
+        ball.transform.localPosition = ballpos;
+        ball_rb.velocity = Vector3.zero;
+        ball_rb.angularVelocity = Vector3.zero;
+
+        Vector3 zonepos = new Vector3(Random.Range(-30.0f, 30.0f), 0, Random.Range(-30.0f, 30.0f));
+        while(Vector3.Distance(ballpos, zonepos) < 10.0f)
+        {
+            zonepos = new Vector3(Random.Range(-30.0f, 30.0f), 0, Random.Range(-30.0f, 30.0f));
+        }
+        zone.transform.localPosition = zonepos;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.InverseTransformDirection(rb.velocity).z);
+        sensor.AddObservation(transform.InverseTransformPoint(ball.position).x);
+        sensor.AddObservation(transform.InverseTransformPoint(ball.position).z);
+        sensor.AddObservation(transform.InverseTransformPoint(zone.position).x);
+        sensor.AddObservation(transform.InverseTransformPoint(zone.position).z);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxis("Throttle");
+        continuousActions[1] = Input.GetAxis("Steering");
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        wheelVehicle.throttle = actions.ContinuousActions[0];
+        wheelVehicle.steeringInput = actions.ContinuousActions[1];
+
+        if (rb.velocity.magnitude < 0.1f)
+        {
+            AddReward(-rb.velocity.magnitude * 0.01f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("ball"))
+        {
+            AddReward(0.1f);
+        }
+    }
+}
